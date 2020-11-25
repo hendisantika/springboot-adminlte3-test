a6c20f142ddb884a6a26b23dbe451fc7cc0a5c3d
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-25 08:02:13 +0700

    #6 - Add WebSecurityConfig

---
+++ src/main/java/com/hendisantika/springbootadminlte3/config/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.hendisantika.springbootadminlte3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-adminlte3
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 25/11/20
+ * Time: 08.01
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/adminlte/**", "/dist/**", "/plugins/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .failureUrl("/login?error=true")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and().csrf().disable();
+    }
+}
